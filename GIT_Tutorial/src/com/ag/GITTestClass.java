930b4ff7ff6fbcc88e843fc96909c5ecadfc7f87
Shirshendu Shekhar Das <shirshendu.das@ariveguru.com>
2013-02-15 12:21:49 +0530

    FIFTH COMMIT

--- GIT_Tutorial/src/com/ag/GITTestClass.java
+++ GIT_Tutorial/src/com/ag/GITTestClass.java
@@ -14,4 +14,10 @@ public class GITTestClass {
 	public void setUsername(String username) {
 		this.username = username;
 	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
 }
