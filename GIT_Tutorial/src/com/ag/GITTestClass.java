295522b47494ea8c1c8d7243a29ca3c6f30c987a
AG <AG@AG3>
2013-02-15 11:57:34 +0530

    Third Commit

--- GIT_Tutorial/src/com/ag/GITTestClass.java
+++ GIT_Tutorial/src/com/ag/GITTestClass.java
@@ -1,10 +1,17 @@
 package com.ag;
 
 public class GITTestClass {
+	private String username;
 	static{
 		System.out.println("Class Loaded");
 	}
 	public GITTestClass() {
 		System.out.println("Object Created");
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
 	}
 }
